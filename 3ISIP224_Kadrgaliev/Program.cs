091bcd29d0ebe337d177d8791727e019fb58cd21
Шамиль Кадргалиев <darkworker1488@gmail.com>
2026-09-09 10:50:27 +0300

    Добавить ввод и проверку количества финансовых операций

--- 3ISIP224_Kadrgaliev/Program.cs
+++ 3ISIP224_Kadrgaliev/Program.cs
@@ -10,6 +10,22 @@ namespace _3ISIP224_Kadrgaliev
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество операций, которые будут записаны: ");
+            int countFinTran = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            if (countFinTran < 2)
+            {
+                Console.WriteLine("Вы ввели слишком мало операций!");
+            }
+            else if (countFinTran > 40)
+            {
+                Console.WriteLine("Вы ввели слишком много операций!");
+            }
+            else
+            {
+                Console.WriteLine("Введите свои траты, которые будут записаны: ");
+                Console.Write("Пример: (Влажные салфетки \"Лента\"; 235)");
+            }
         }
     }
 }
