270d9a9898abe87f8778eee5baff02e78145f37a
Шамиль Кадргалиев <darkworker1488@gmail.com>
2026-09-10 09:03:42 +0300

    Сделал функцию вывода данных, вывода статистики, сортировку пузырьком и немного изменил меню

--- 3ISIP224_Kadrgaliev/Program.cs
+++ 3ISIP224_Kadrgaliev/Program.cs
@@ -26,6 +26,7 @@ namespace _3ISIP224_Kadrgaliev
                 List<(string Name, decimal Amount)> expenses = new List<(string Name, decimal Amount)>();
                 Console.WriteLine("Введите свои траты, которые будут записаны: ");
                 Console.Write("Пример: (Влажные салфетки \"Лента\"; 235)");
+                Console.WriteLine();
                 for (int i = 0; i < countFinTran; i++)
                 {
                     Console.WriteLine();
@@ -36,49 +37,103 @@ namespace _3ISIP224_Kadrgaliev
                     decimal amount = Convert.ToDecimal(Console.ReadLine());
                     expenses.Add((name, amount));
                 }
-                Console.WriteLine("------ Меню ------");
-                Console.WriteLine("1. Вывод данных ");
-                Console.WriteLine("2. Статистика (среднее, максимальное, минимальное, сумма) ");
-                Console.WriteLine("3. Сортировка по цене (пузырьковая сортировка) ");
-                Console.WriteLine("4. Конвертация валюты (пользователь вводит курс или выбирает из списка) ");
-                Console.WriteLine("5. Введите свои траты, которые будут записаны ");
-                Console.WriteLine("0. Выход ");
-                Console.WriteLine();
-                Console.Write("Ваш выбор: ");
-                int choise = Convert.ToInt32(Console.ReadLine());
-                switch (choise)
+                bool programIsRunning = true;
+                while (programIsRunning)
                 {
-                    case 1:
-                        Console.WriteLine("Вы выбрали вывод данных");
-                        Console.WriteLine();
-                        Console.WriteLine("Список трат:");
-                        for (int i = 0; i < expenses.Count; i++)
-                        {
-                            Console.WriteLine(
-                                $"{i + 1}. {expenses[i].Name} — " +
-                                $"{expenses[i].Amount:F2} руб.");
-                        }
-                        break;
-                    case 2:
-                        Console.WriteLine("Вы выбрали статистику");
-                        break;
-                    case 3:
-                        Console.WriteLine("Вы выбрали сортировку по цене");
-                        break;
-                    case 4:
-                        Console.WriteLine("Вы выбрали конвертацию валюты");
-                        break;
-                    case 5:
-                        Console.WriteLine("Вы выбрали ввод своих трат");
-                        break;
-                    case 0:
-                        Console.WriteLine("Выход из программы");
-                        break;
-                    default:
-                        Console.WriteLine("Неверный выбор. Попробуйте снова.");
-                        break;
+                    Console.WriteLine();
+                    Console.WriteLine("------ Меню ------");
+                    Console.WriteLine("1. Вывод данных ");
+                    Console.WriteLine("2. Статистика (среднее, максимальное, минимальное, сумма) ");
+                    Console.WriteLine("3. Сортировка по цене (пузырьковая сортировка) ");
+                    Console.WriteLine("4. Конвертация валюты (пользователь вводит курс или выбирает из списка) ");
+                    Console.WriteLine("5. Введите свои траты, которые будут записаны ");
+                    Console.WriteLine("0. Выход ");
+                    Console.WriteLine();
+                    Console.Write("Ваш выбор: ");
+                    int choise = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (choise)
+                    {
+                        case 1:
+                            Console.Clear();
+                            Console.WriteLine("Вы выбрали вывод данных");
+                            Console.WriteLine();
+                            ShowExpenses(expenses);
+                            break;
+                        case 2:
+                            Console.Clear();
+                            Console.WriteLine("Вы выбрали статистику");
+                            ShowStatistic(expenses);
+                            break;
+                        case 3:
+                            Console.WriteLine("Вы выбрали сортировку по цене");
+                            BubbleSort(expenses);
+                            break;
+                        case 4:
+                            Console.WriteLine("Вы выбрали конвертацию валюты");
+                            break;
+                        case 5:
+                            Console.WriteLine("Вы выбрали ввод своих трат");
+                            break;
+                        case 0:
+                            Console.WriteLine("Выход из программы");
+                            programIsRunning = false;
+                            break;
+                        default:
+                            Console.WriteLine("Неверный выбор. Попробуйте снова.");
+                            break;
+                    }
                 }
-               
+            }
+        }
+
+        static void ShowExpenses(List<(string Name, decimal Amount)> expenses)
+        {
+            Console.WriteLine("Список трат:");
+            for (int i = 0; i < expenses.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {expenses[i].Name} — " + $"{expenses[i].Amount:F2} руб.");
+            }
+        }   
+        static void ShowStatistic(List<(string Name, decimal Amount)> expenses)
+        {
+            decimal sum = 0;
+            decimal min = expenses[0].Amount;
+            decimal max = expenses[0].Amount;
+            foreach (var expense in expenses)
+            {
+                sum += expense.Amount;
+
+                if (expense.Amount < min)
+                {
+                    min = expense.Amount;
+                }
+
+                if (expense.Amount > max)
+                {
+                    max = expense.Amount;
+                }
+            }
+            decimal average = sum / expenses.Count;
+            Console.WriteLine($"Сумма: {sum:F2} руб.");
+            Console.WriteLine($"Среднее: {average:F2} руб.");
+            Console.WriteLine($"Минимум: {min:F2} руб.");
+            Console.WriteLine($"Максимум: {max:F2} руб.");
+        }
+
+        static void BubbleSort(List<(string Name, decimal Amount)> expenses)
+        {
+            for (int i = 0; i < expenses.Count - 1; i++)
+            {
+                for (int j = 0; j < expenses.Count - i - 1; j++)
+                {
+                    if (expenses[j].Amount > expenses[j + 1].Amount)
+                    {
+                        var temporary = expenses[j];
+                        expenses[j] = expenses[j + 1];
+                        expenses[j + 1] = temporary;
+                    }
+                }
             }
         }
     }
