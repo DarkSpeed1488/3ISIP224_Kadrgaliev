ca4fe6fa4318c6bc90ded50e51eb2182b8b00289
Шамиль Кадргалиев <darkworker1488@gmail.com>
2026-09-16 00:39:22 +0300

    Добавил наполнение списка тестовыми товарами. Создал метод  для детального вывода одного товара. Создал метод для перебора всего списка с проверкой на пустоту.

--- 3ISIP224_Kadrgaliev/Program.cs
+++ 3ISIP224_Kadrgaliev/Program.cs
@@ -8,9 +8,9 @@ namespace _3ISIP224_Kadrgaliev
 {
     enum Category
     {
-        Food,
-        Electronics,
-        Clothes
+        Еда,
+        Электроника,
+        Одежда
     }
 
     class Product
@@ -31,12 +31,45 @@ namespace _3ISIP224_Kadrgaliev
             Quantity = quantity;
             ProductCategory = category;
         }
+
     }
+
 
     internal class Program
     {
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product("Хлеб", 87, 33, Category.Еда));
+            products.Add(new Product("Молоко", 117, 41, Category.Еда));
+            products.Add(new Product("Телефон", 35000, 5, Category.Электроника));
+            products.Add(new Product("Наушники", 5000, 13, Category.Электроника));
+            products.Add(new Product("Футболка", 1500, 12, Category.Одежда));
+            ShowAllProducts(products);
+        }
+        static void ShowInfo(Product product)
+        {
+            Console.WriteLine($"Код: {product.ID}");
+            Console.WriteLine($"Название: {product.Name}");
+            Console.WriteLine($"Цена: {product.Price} руб.");
+            Console.WriteLine($"Количество: {product.Quantity}");
+            Console.WriteLine($"На складе: {(product.InStock ? "Да" : "Нет")}");
+            Console.WriteLine($"Категория: {product.ProductCategory}");
+            Console.WriteLine();
+        }
+
+        static void ShowAllProducts(List<Product> products)
+        {
+            if (products.Count == 0)
+            {
+                Console.WriteLine("Список товаров пуст.");
+                return;
+            }
+            foreach (Product product in products)
+            {
+                ShowInfo(product);
+            }
         }
     }
 }
