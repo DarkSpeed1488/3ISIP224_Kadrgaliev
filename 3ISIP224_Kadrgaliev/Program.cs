df50a4bc8d8ca2d465fa727ba010a8a97109dc7d
Шамиль Кадргалиев <darkworker1488@gmail.com>
2026-09-15 21:53:38 +0300

    Добавил модель класса Product

--- 3ISIP224_Kadrgaliev/Program.cs
+++ 3ISIP224_Kadrgaliev/Program.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace _3ISIP224_Kadrgaliev
 {
+    class Product
+    {
+        private static int nextID = 1;
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public Category ProductCategory { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
