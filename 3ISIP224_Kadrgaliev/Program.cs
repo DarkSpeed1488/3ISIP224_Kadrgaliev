e4b4d2fb0da8948c668d5659c0c6ab3541e82f35
Шамиль Кадргалиев <darkworker1488@gmail.com>
2026-09-26 16:40:20 +0300

    Добавил класс Text

--- 3ISIP224_Kadrgaliev/Program.cs
+++ 3ISIP224_Kadrgaliev/Program.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace _3ISIP224_Kadrgaliev
 {
+    public class Text
+    {
+        public string Text { get; set; }
+        public int WordCount { get; set; }
+        public string ShortestWord { get; set; }
+        public string LongestWord { get; set; }
+        public int SentenceCount { get; set; }
+        public int VowelCount { get; set; }
+        public int ConsonantCount { get; set; }
+    }
     internal class Program
     {
         static void Main(string[] args)
